2b326c41d9017522b8ed5057865761035ad9273f
Arthur <arthurolis41@gmail.com>
2022-11-08 17:25:13 -0300

    description features

--- src/components/Elements.jsx
+++ src/components/Elements.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import WeatherElements from "./WeatherElements";
-// import WeatherContext from "../data/context";
+import WeatherContext from "../data/context";
 import '../styles/elements.css';
 
 function Elements() {
+
+  const {
+    api
+  } = useContext(WeatherContext);
+
+  const description = api.weather[0].description;
 
   return (
     <section
       className="section-elements"
     >
+      <aside
+        className="aside-clime"
+      >
+        <span>{`${description.toUpperCase()}`}</span>
+      </aside>
+      <aside
+        className="aside-elements"
+      >
         <WeatherElements
           icon='humidity'
         />
         <WeatherElements
           icon='wind'
         />
+      </aside>
+      <aside
+        className="aside-sun"
+      >
+        <span>Sunrise    sunset</span>
+      </aside>
     </section>
   );
 }
