0684326806699127056b269aecd92e71a37c6e2f
Arthur <arthurolis41@gmail.com>
2022-11-07 23:39:29 -0300

    replacing icons

--- src/data/icons.js
+++ src/data/icons.js
@@ -2,7 +2,7 @@ import sunny from '../icons/sunny.png';
 import cloudsNight from '../icons/cloudsNight.png'
 import thunderstorm from '../icons/thunderstorm.png'
 import rain from '../icons/rain.png';
-import snow from '../icons/snow.png';
+import snow from '../icons/snows.png';
 import mist from '../icons/mist.png';
 
 export const weatherIcon = [
